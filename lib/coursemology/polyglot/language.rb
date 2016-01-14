a4470e5b00ebc6155d90db5fb1548f834735b1d4
Joel Low <joel@joelsplace.sg>
2016-01-14 23:19:10 +0800

    Make language definitions self-contained.
    This adds extension points to Coursemology::Polyglot::Language such that all definitions for a language is contained within the polyglot gem. This also allows extensions to be added to concrete languages in API consumers.

--- lib/coursemology/polyglot/language.rb
+++ lib/coursemology/polyglot/language.rb
@@ -8,11 +8,50 @@ else
   class Coursemology::Polyglot::Language; end
 end
 
+# An abstract language. This is a class which can represent a family of languages. Languages become
+# concrete and can be used when that language includes +Coursemology::Polyglot::ConcreteLanguage+.
+#
+# Languages define their own scripts and stylesheets needed to syntax highlight code.
+#
+# Each subclass represents a language ancestry, such as differing language versions (see the Python
+# language definition.) Derived languages can be defined at runtime to utilise the syntax
+# highlighting capabilities of the root language, while requiring a separate runtime environment to
+# run programs written in the derived language.
+#
+# Do *NOT* remove languages after they have been defined because the types specified in this
+# library can have references stored in code (e.g. in a database)
 class Coursemology::Polyglot::Language
   extend ActiveSupport::Autoload
 
   eager_autoload do
     autoload :Python
+  end
+
+  # Marks the current class as a concrete language.
+  #
+  # Concrete languages can be instantiated and used.
+  #
+  # @param [String] display_name The display name for the language
+  def self.concrete_language(display_name)
+    include Coursemology::Polyglot::ConcreteLanguage
+    extend Coursemology::Polyglot::ConcreteLanguage::ClassMethods
+
+    concrete_class_methods = Module.new do
+      define_method(:display_name) do
+        display_name
+      end
+    end
+
+    extend concrete_class_methods
+  end
+
+  # Determines the concrete language subclasses of this language.
+  #
+  # @return [Array<Class>]
+  def self.concrete_languages
+    descendants.select do |klass|
+      klass.ancestors.include?(Coursemology::Polyglot::ConcreteLanguage)
+    end
   end
 
   # Gets the display name of the language.
--- lib/coursemology/polyglot/language/python.rb
+++ lib/coursemology/polyglot/language/python.rb
@@ -1,17 +1,9 @@
 class Coursemology::Polyglot::Language::Python < Coursemology::Polyglot::Language
   class Python2Point7 < Coursemology::Polyglot::Language::Python
-    class << self
-      def display_name
-        'Python 2.7'
-      end
-    end
+    concrete_language 'Python 2.7'
   end
 
   class Python3Point4 < Coursemology::Polyglot::Language::Python
-    class << self
-      def display_name
-        'Python 3.4'
-      end
-    end
+    concrete_language 'Python 3.4'
   end
 end
