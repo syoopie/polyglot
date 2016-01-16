a6ca2086ce8ad5e0a47d205ba6eb7fc47f852510
Joel Low <joel@joelsplace.sg>
2016-01-16 14:37:11 +0800

    Implement algorithmic generation of Docker image names given a programming language name.
    This is needed to map a language to a suitable Docker image to use to run programs in that language.

--- lib/coursemology/polyglot/concrete_language/class_methods.rb
+++ lib/coursemology/polyglot/concrete_language/class_methods.rb
@@ -4,4 +4,26 @@
 # This is deliberately defined so that consumers of this library can inject methods into all
 # concrete languages' classes.
 module Coursemology::Polyglot::ConcreteLanguage::ClassMethods
+  # The default algorithm for translating a language to a corresponding Docker image name.
+  #
+  # This will:
+  #  1. Strip the +Coursemology::Polyglot::Language+ prefix.
+  #  2. Underscore the name.
+  #  3. Replace *n*Point*n* strings with dots (e.g. Python2Point7 to Python2.7).
+  #  4. Replace slashes with dashes.
+  def docker_image
+    # Remove the Coursemology::Polyglot::Language prefix
+    class_name = name
+    class_name = class_name.sub(/^Coursemology::Polyglot::Language::/, '')
+
+    # Use the underscored name
+    class_name = class_name.underscore
+
+    # Replace the "_point" string with a decimal
+    class_name.gsub!(/(?<=[\d])_point(?=[\d])/, '.')
+
+    # Replace slashes with dashes
+    class_name.tr!('/', '-')
+    class_name
+  end
 end
---
+++ spec/coursemology/polyglot/concrete_language_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+RSpec.describe Coursemology::Polyglot::ConcreteLanguage do
+  class self::DummyLanguage < Coursemology::Polyglot::Language
+    concrete_language 'Dummy Concrete Language'
+  end
+
+  class self::DummyOverriddenNameLanguage < self::DummyLanguage
+    concrete_language 'Dummy Overridden Concrete Language'
+
+    def self.name
+      'Coursemology::Polyglot::Language::Dummy::OverriddenNameLanguage'.freeze
+    end
+  end
+
+  describe '.docker_image' do
+    it 'removes the Coursemology::Polyglot::Language prefix' do
+      expect(self.class::DummyOverriddenNameLanguage.docker_image).to \
+        start_with('dummy')
+    end
+
+    it 'preserves the nesting of namespaces' do
+      expect(self.class::DummyOverriddenNameLanguage.docker_image).to \
+        eq('dummy-overridden_name_language')
+    end
+
+    context 'when the name contains Point surrounded by numerals' do
+      class self::DummyLanguage1Point0 < self::DummyLanguage; end
+
+      it 'converts "Point" to dots' do
+        expect(self.class::DummyLanguage1Point0.docker_image).to end_with('dummy_language1.0')
+      end
+    end
+
+    context 'when the name contains Point but not surrounded by numerals' do
+      class self::DummyLanguagePoint < self::DummyLanguage; end
+
+      it 'preserves "Point"' do
+        expect(self.class::DummyLanguagePoint.docker_image).to end_with('dummy_language_point')
+      end
+    end
+  end
+end
