ae926e2db58e77558fdcf0fdd92e86bf488c2c7b
Joel Low <joel@joelsplace.sg>
2016-01-16 21:25:24 +0800

    Fix the name of the Python images to use Docker tags.

--- lib/coursemology/polyglot/language/python.rb
+++ lib/coursemology/polyglot/language/python.rb
@@ -1,9 +1,9 @@
 class Coursemology::Polyglot::Language::Python < Coursemology::Polyglot::Language
   class Python2Point7 < Coursemology::Polyglot::Language::Python
-    concrete_language 'Python 2.7'
+    concrete_language 'Python 2.7', docker_image: 'python:2.7'
   end
 
   class Python3Point4 < Coursemology::Polyglot::Language::Python
-    concrete_language 'Python 3.4'
+    concrete_language 'Python 3.4', docker_image: 'python:3.4'
   end
 end
