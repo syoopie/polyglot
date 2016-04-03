bd3daf61eae60262b783a6c84ad7a4b91ed64489
Joel Low <joel@joelsplace.sg>
2016-04-03 21:18:51 +0800

    Fix casing for the display name.

--- lib/coursemology/polyglot/language/java_script.rb
+++ lib/coursemology/polyglot/language/java_script.rb
@@ -1,4 +1,4 @@
 class Coursemology::Polyglot::Language::JavaScript < Coursemology::Polyglot::Language
   syntax_highlighter 'javascript'
-  concrete_language 'javascript', docker_image: 'javascript'
+  concrete_language 'JavaScript', docker_image: 'javascript'
 end
