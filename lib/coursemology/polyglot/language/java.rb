f6ba929c11188405181924a89eb855c7cd9d7566
trangmei <trangmei@gmail.com>
2019-10-08 15:46:31 +0800

    Add Java 11 and versioning for Java

--- lib/coursemology/polyglot/language/java.rb
+++ lib/coursemology/polyglot/language/java.rb
@@ -1,4 +1,11 @@
 class Coursemology::Polyglot::Language::Java < Coursemology::Polyglot::Language
   syntax_highlighter 'java'
-  concrete_language 'Java', docker_image: 'java'
+
+  class Java8 < Coursemology::Polyglot::Language::Java
+    concrete_language 'Java 8', docker_image: 'java:8'
+  end
+
+  class Java11 < Coursemology::Polyglot::Language::Java
+    concrete_language 'Java 11', docker_image: 'java:11'
+  end
 end
